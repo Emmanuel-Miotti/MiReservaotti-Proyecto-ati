30b172031e08cf7574a495d0e75ed7f94098a6cd
Emmanuel-Miotti <109709518+Emmanuel-Miotti@users.noreply.github.com>
2024-08-31 21:55:26 -0300

    .

--- MiReservaOtti-main/frontend/web/src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-// Utilidades generales para la aplicación
-
-// Función para construir URLs completas a partir de un path
-export function getFullURL(path) {
-    return `http://localhost:8000${path}`;
-}
